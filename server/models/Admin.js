c23b79b04b4c9388bf9b0d7ef293f0561db30663
Jhonatan Calle <107059096+JcalleA@users.noreply.github.com>
2022-11-29 19:18:57 -0500

    Delete server directory

--- server/models/Admin.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const AdminSchema = mongoose.Schema({
-    password: { type: String, require: true },
-});
-
-module.exports = mongoose.model("Administrador", AdminSchema);
