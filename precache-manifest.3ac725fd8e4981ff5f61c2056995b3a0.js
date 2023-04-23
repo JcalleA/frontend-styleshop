16120636b12534e5cec874653bcdf1e65d3d1e13
JcalleA <kalle-1413@hotmail.com>
2023-04-23 08:31:24 -0500

    Updates

---
+++ precache-manifest.3ac725fd8e4981ff5f61c2056995b3a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "15938f6f476c78eca6fd",
+    "url": "/frontend-styleshop/static/js/0.dc6d9654.chunk.js"
+  },
+  {
+    "revision": "13664b2c184227e02d9b",
+    "url": "/frontend-styleshop/static/css/main.b01c1f03.chunk.css"
+  },
+  {
+    "revision": "13664b2c184227e02d9b",
+    "url": "/frontend-styleshop/static/js/main.e4b842d5.chunk.js"
+  },
+  {
+    "revision": "b5ff2ab18844b5e76c9d",
+    "url": "/frontend-styleshop/static/js/runtime~main.9f1055f4.js"
+  },
+  {
+    "revision": "603edf714b261d50a8bb",
+    "url": "/frontend-styleshop/static/css/3.2b0a98ab.chunk.css"
+  },
+  {
+    "revision": "603edf714b261d50a8bb",
+    "url": "/frontend-styleshop/static/js/3.6f19091c.chunk.js"
+  },
+  {
+    "revision": "65d58dcaa60f6a2c848d",
+    "url": "/frontend-styleshop/static/js/4.0dd4907d.chunk.js"
+  },
+  {
+    "revision": "4501f3c33396ae573d5e",
+    "url": "/frontend-styleshop/static/js/5.34fe0d42.chunk.js"
+  },
+  {
+    "revision": "5c7e2f68c043abf456c2",
+    "url": "/frontend-styleshop/static/js/6.bbaf5825.chunk.js"
+  },
+  {
+    "revision": "c0e17b92dda8245a93b30a95fbec4c50",
+    "url": "/frontend-styleshop/index.html"
+  }
+];
